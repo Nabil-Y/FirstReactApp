3aa2925b6a278b668c272d228f5d6a77964a9110
Nabil Yassine <nabil.yassine.dev@gmail.com>
2021-11-30 16:24:58 +0100

    Finish React Build

---
+++ src/emojipedia.js
@@ -0,0 +1,26 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "🤖",
+      name: "Robot",
+      meaning:
+        "The head of a classic robot. Commonly depicted as a vintage, tin toy robot with circular eyes, knobs for ears, a light and/or antennae atop its head, and a grill-like mouth similar to a grimace."
+    },
+    {
+      id: 2,
+      emoji: "🧐",
+      name: "Face with Monocle",
+      meaning:
+        "A yellow face with furrowed eyebrows wearing a monocle. May show that someone is pondering, considering, or questioning something."
+    },
+    {
+      id: 3,
+      emoji: "🤝",
+      name: "Handshake",
+      meaning:
+        "Two hands performing a handshake gesture, indicating a cordial greeting between friends or associates."
+    },
+  ];
+  
+  export default emojipedia;
+  
