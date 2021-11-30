d68552f9bac17a4da0b9d997f8de76d716cf503d
Nabil Yassine <nabil.yassine.dev@gmail.com>
2021-12-01 00:02:03 +0100

    Final version of project

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -20,6 +20,48 @@ const emojipedia = [
       meaning:
         "Two hands performing a handshake gesture, indicating a cordial greeting between friends or associates."
     },
+    {
+      id: 4,
+      emoji: "😩",
+      name: "Weary Face",
+      meaning:
+        "A yellow face with closed eyes, furrowed brows, and a broad, open frown, as if distraught to the point of giving up. Usually shown with upper teeth and sometimes tongue."
+    },
+    {
+      id: 5,
+      emoji: "🥺",
+      name: "Pleading Face",
+      meaning:
+        "A yellow face with furrowed eyebrows, a small frown, and large, “puppy dog” eyes, as if begging or pleading. May also represent adoration or feeling touched by a loving gesture."
+    },
+    {
+      id: 6,
+      emoji: "😺",
+      name: "Grinning Cat",
+      meaning:
+        "A cartoon cat variant of 😀 Grinning Face. Depicted as yellow on major platforms."
+    },
+    {
+      id: 7,
+      emoji: "🖖",
+      name: "Vulcan Salute",
+      meaning:
+        "A raised hand, with the fingers separated between the ring finger and the middle finger."
+    },
+    {
+      id: 8,
+      emoji: "👶",
+      name: "Baby",
+      meaning:
+        "The face of a young infant (baby). Shows the face of a baby with a small amount of hair on most platforms, while some variations show a pacifier in its mouth. "
+    },
+    {
+      id: 9,
+      emoji: "🏃",
+      name: "Person Running",
+      meaning:
+        "A person running, with a large stride and arms outstretched. May be wearing a running shirt."
+    }
   ];
   
   export default emojipedia;
